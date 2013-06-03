6f0ac7986158063f5dd2bf9684ac59cc6e58a2fa
Dave Thompson <dthompson@rallydev.com>
2013-06-03 15:22:35 -0600

    Display color to indicate build status.

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-AppTemplate for Rally SDK
-=========================
+RallyBuildLight
+===============
 
 ## Overview
 
-This Rakefile can be used to create a skeleton Rally app for use with Rally's App SDK.  You must have Ruby 1.9 and the rake gem installed.
+Displays status of Jenkins builds.
 
-The normal workflow for creating an App is to start by creating an App with the new task.
+## Useage
 
-Available tasks are:
+* Compile:
 
-    rake new[app_name,sdk_version,server]   # Create an app with the provided name (and optional SDK version and rally server [default: https://rally1.rallydev.com])
-    rake debug                              # Build a debug version of the app, useful for local development. 
-    rake build                              # Build a deployable app which includes all JavaScript and CSS resources inline. Use after you app is working as you intend so that it can be copied into Rally.
-    rake clean                              # Clean all generated output
-    rake jslint                             # Run jslint on all JavaScript files used by this app
-    rake deploy                             # Deploy the app to a Rally server
-    rake deploy:debug                       # Deploy the debug app to a Rally server
-    rake deploy:info                        # Display deployment information
-    
-You can find more information on installing Ruby and using rake tasks to simplify app development here: https://rally1.rallydev.com/apps/2.0p3/doc/#!/guide/appsdk_20_starter_kit
+```grunt```
+
+* Copy and paste ```/deploy.html``` into Custom HTML app.
+
+* Edit app settings and enter Jenkins job url. Use commas to separate multiple jobs.
 
 ## License
 
---
+++ src/App.js
@@ -0,0 +1,125 @@
+Ext.define('RallyBuildLight', {
+    extend: 'Rally.app.App',
+    requires: [
+        'Ext.data.JsonP',
+        'Ext.util.TaskManager',
+        'Rally.ui.NumberField'
+    ],
+    componentCls: 'app',
+
+    statuses: {
+        INCOMPLETE: 'INCOMPLETE',
+        SUCCESS: 'SUCCESS'
+    },
+
+    layout: 'fit',
+
+    items: [{
+        itemId: 'statusIndicator',
+        border: false,
+        bodyBorder: false
+    }],
+
+    launch: function() {
+        this.setLoading(true);
+        this._startPoll();
+    },
+
+    getSettingsFields: function() {
+        return [{
+            xtype: 'rallynumberfield',
+            name: 'interval',
+            label: 'Refresh Interval (seconds)',
+            value: 15
+        }, {
+            xtype: 'rallytextfield',
+            name: 'jobUrl',
+            label: 'Jenkins Job URL',
+            value: 'http://jenkins.server/job/jobname',
+            width: 400
+        }];
+    },
+
+    _startPoll: function() {
+        if (this.pollTask) {
+            this.pollTask.destroy();
+        }
+
+        this.pollTask = Ext.util.TaskManager.start({
+            run: this._poll,
+            scope: this,
+            interval: (parseInt(this.getSettings().interval, 10) || 15) * 1000
+        }) ;
+    },
+
+    _poll: function() {
+        var jobStatuses = this._createJobStatuses(),
+            me = this;
+
+        _.each(jobStatuses, function(jobStatus) {
+            Ext.data.JsonP.request({
+                url: jobStatus.apiUrl,
+                callbackKey: 'jsonp',
+                success: function(response) {
+                    jobStatus.data = response;
+                    me._checkJobStatus(jobStatuses);
+                },
+                failure: function() {
+                    if (!me.notifiedError) {
+                        Rally.ui.notify.Notifier.showError({message: 'Unable to contact Jenkins: ' + jobStatus.apiUrl});
+                        me.notifiedError = true; // avoid spewing notifications on every poll.
+                    }
+                }
+            });
+        });
+    },
+
+    _checkJobStatus: function(jobStatuses) {
+        var status = this._getJobStatus(jobStatuses),
+            statusIndicator;
+
+        if (status !== this.statuses.INCOMPLETE) {
+            statusIndicator = this.down('#statusIndicator');
+            if (this.currentStatus) {
+                statusIndicator.removeCls(this.currentStatus);
+            }
+            statusIndicator.addCls(status);
+            this.currentStatus = status;
+            this.jobStatuses = jobStatuses;
+            this.setLoading(false);
+        }
+    },
+
+    _getJobStatus: function(jobStatuses) {
+        var i, jobStatus, result;
+
+        for (i = 0; i < jobStatuses.length; i++) {
+            jobStatus = jobStatuses[i];
+
+            if (!jobStatus.data || !jobStatus.data.result) {
+                return this.statuses.INCOMPLETE;
+            }
+
+            if (jobStatus.data.result !== this.statuses.SUCCESS) {
+                return jobStatus.data.result;
+            }
+        }
+
+        return this.statuses.SUCCESS;
+    },
+
+    _getApiUrl: function(jobUrl) {
+        return jobUrl.replace(/^\s+|\s+$/g, '') + '/lastCompletedBuild/api/json';
+    },
+
+    _createJobStatuses: function(jobUrls) {
+        var jobUrls = (this.getSettings().jobUrl || '').split(',');
+
+        return _.map(jobUrls, function(jobUrl) {
+            return {
+                url: jobUrl,
+                apiUrl: this._getApiUrl(jobUrl)
+            };
+        }, this);
+    }
+});
